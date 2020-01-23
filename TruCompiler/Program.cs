e60e68ea7ea1c4d24b7cea769c3d4d98283a5a9a
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2020-01-23 01:44:10 -0500

    [#1] Initial Commit

---
+++ TruCompiler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TruCompiler
+{
+    /// <summary>
+    /// Main program class to initialize the passed arguments and run the driver.
+    /// </summary>
+    class Program
+    {
+        private Program(string[] args)
+        {
+
+        }
+        public static void Main(string[] args)
+        {
+            new Program(args).Run(); 
+        }
+
+        private void Run()
+        {
+            //Call driver with parsed args
+        }
+    }
+}
