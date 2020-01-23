81cbbd51c9ee58da8f4a63bc2b2194924b00cdfa
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2020-01-23 01:44:35 -0500

    [#1] Adding backbone of the driver class and an empty lexical analyzer

---
+++ TruCompiler/Driver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TruCompiler
+{
+    /// <summary>
+    /// Driver class that is responsible for the main logic of TruCompiler.
+    /// The below class includes calling and processing the lexical analyzer
+    /// </summary>
+    class Driver
+    {
+        public string[] InputFiles { get; private set; }
+        public string OutputPath { get; private set; }
+        public Driver(string[] inputFiles, string outputPath)
+        {
+            InputFiles = inputFiles;
+            OutputPath = outputPath;
+        }
+        private void Compile()
+        {
+
+        }
+    }
+}
---
+++ TruCompiler/Lexical Analyzer/LexicalAnalyzer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TruCompiler.Lexical_Analyzer
+{
+    class LexicalAnalyzer
+    {
+    }
+}
