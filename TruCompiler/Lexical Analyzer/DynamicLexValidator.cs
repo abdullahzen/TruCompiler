077c4d09bb44b2668f1055f7189ce701ca29d1e4
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2020-01-28 14:22:13 -0500

    [#1] Created base validator for identifier, float, integer

---
+++ TruCompiler/Lexical Analyzer/DynamicLexValidator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static TruCompiler.Lexical_Analyzer.Tokens;
+
+namespace TruCompiler.Lexical_Analyzer
+{
+    public class DynamicLexValidator : IValidator<Token>
+    {
+        public Token Validate(string value, int i, string type)
+        {
+            switch (type)
+            {
+                case "Integer":
+                    return ValidateInteger(value, i);
+                case "Float":
+                    return ValidateFloat(value, i);
+                case "Identifier":
+                    return ValidateIdentifier(value, i);
+                default:
+                    throw new Exception("Argument type cannot be empty or null", new ArgumentNullException(type));
+            }
+        }
+
+        private Token ValidateInteger(string value, int i)
+        {
+            return new Token();
+        }
+
+        private Token ValidateIdentifier(string value, int i)
+        {
+            return new Token();
+
+        }
+
+        private Token ValidateFloat(string value, int i)
+        {
+            return new Token();
+
+
+        }
+
+       
+    }
+}
---
+++ TruCompiler/Lexical Analyzer/IValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace TruCompiler.Lexical_Analyzer
+{
+    public interface IValidator<T>
+    {
+        public T Validate(string value, int i, string type);
+    }
+}
