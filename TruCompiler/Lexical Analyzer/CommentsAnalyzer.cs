65aea1a6686b33b2925ac7c98db4962e0e48e0ca
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2020-01-29 00:59:43 -0500

    [#1] Created block comments analyzer

--- TruCompiler/Lexical Analyzer/CommentsAnalyzer.cs
+++ TruCompiler/Lexical Analyzer/CommentsAnalyzer.cs
@@ -41,76 +41,68 @@ namespace TruCompiler.Lexical_Analyzer
             });
         }
 
-        public static string CheckBlockComments(string line, ref IList<Token?> tokens, int i)
+        public static void AddBlockCommentContent(string comment, ref IList<Token?> tokens, int i)
         {
-
-            // block comment
-            if (line.StartsWith("/*") && line.EndsWith("*/"))
+            tokens.Add(new Token()
             {
-                tokens.Add(CreateToken(line.Substring(0, 2), i));
-                tokens.Add(new Token()
-                {
-                    Lexeme = Lexeme.blockcmt,
-                    Value = line,
-                    Location = i,
-                    IsValid = true
-                });
-                tokens.Add(CreateToken(line.Substring(line.Length - 2, 2), i));
-                return string.Empty;
-            }
-            //block cmt start and code after
-            else if (line.StartsWith("/*") && line.Contains("*/"))
-            {
-                tokens.Add(CreateToken(line.Substring(0, 2), i));
-                string[] splitted = line.Split("*/");
-                tokens.Add(new Token()
-                {
-                    Lexeme = Lexeme.blockcmt,
-                    Value = splitted[0].Substring(1, splitted[0].Length),
-                    Location = i,
-                    IsValid = true
-                });
-                tokens.Add(CreateToken(line.Substring(line.IndexOf("*/"), 2), i));
-                line = line.Substring(line.IndexOf("*/") + 1, (line.Length - line.IndexOf("*/") + 1));
-                return CheckBlockComments(line, ref tokens, i);
-            }
-            // code before block cmt
-            else if (line.Contains("/*") && line.EndsWith("*/"))
-            {
-                tokens.Add(CreateToken(line.Substring(line.LastIndexOf("/*"), 2), i));
-                string[] splitted = line.Split("/*");
-                tokens.Add(new Token()
-                {
-                    Lexeme = Lexeme.blockcmt,
-                    // ^1 notation is for the length - 1 index
-                    // range operator 0..^3 is from 0 to length - 3
-                    Value = splitted[^1][0..^3],
-                    Location = i,
-                    IsValid = true
-                });
-                tokens.Add(CreateToken(line.Substring(line.Length - 2, 2), i));
-                line = line.Substring(line.LastIndexOf("/*") - 1, (line.Length - line.LastIndexOf("/*") + 1));
-                return CheckBlockComments(line, ref tokens, i);
-            }
-            else if (line.Contains("/*") && line.Contains("*/"))
-            {
-
-            }
-            else if (line.Contains("/*"))
-            {
-                //check starts with 
-                //and else
-
-            }
-            else if (line.Contains("*/"))
-            {
-                //check starts with and else
-            }
-            else
-            {
-                return line;
-            }
-            return line;
+                Lexeme = Lexeme.blockcmt,
+                Value = comment,
+                Location = i,
+                IsValid = true
+            });
         }
+
+        public static string CheckBlockComments(string value, ref IList<Token?> tokens, int i, ref bool blockcmt)
+        {
+            string[] splittedComment;
+            if (!blockcmt)
+            {
+                if (value.StartsWith("/*"))
+                {
+                    tokens.Add(CreateToken("/*", i));
+                    blockcmt = true;
+                }
+                else if (value.Contains("/*"))
+                {
+                    splittedComment = value.Split("/*");
+                    tokens.Add(CreateToken(splittedComment[0], i));
+                    tokens.Add(CreateToken("/*", i));
+                    value = String.Join("/*", splittedComment);
+                    value = value.Substring(value.IndexOf("/*"), value.Length - splittedComment[0].Length);
+                    blockcmt = true;
+                }
+            }
+            return value;
+        }
+
+        public static string CheckBlockCommentsEnd(string value, ref IList<Token?> tokens, int i, ref bool blockcmt, bool blockcmtMult)
+        {
+            string[] splittedComment;
+            if (blockcmt || blockcmtMult)
+            {
+                if (value.EndsWith("*/"))
+                {
+                    tokens.Add(CreateToken("*/", i));
+                    blockcmt = false;
+                }
+                else if (value.Contains("*/"))
+                {
+                    splittedComment = value.Split("*/");
+                    if (splittedComment.Length > 1)
+                    {
+                        tokens.Add(CreateToken(splittedComment[1], i));
+                    }
+                    tokens.Add(CreateToken("*/", i));
+                    value = String.Join("*/", splittedComment);
+                    value = value.Substring(0, value.LastIndexOf("*/") + 2);
+                    blockcmt = false;
+                }
+            }
+            return value;
+        }
+
+
+
     }
+         
 }
--- TruCompiler/Lexical Analyzer/LexicalAnalyzer.cs
+++ TruCompiler/Lexical Analyzer/LexicalAnalyzer.cs
@@ -10,6 +10,8 @@ namespace TruCompiler.Lexical_Analyzer
         public static IList<Token?> Tokenize(string[] lines)
         {
             IList<Token?> tokens = new List<Token?>();
+            bool blockcmtMult = false;
+            string blockcmtContent = "";
             if (lines != null && lines.Length != 0)
             {
                 //Loop through all the lines and find the tokens.
@@ -18,13 +20,31 @@ namespace TruCompiler.Lexical_Analyzer
                     int i = k + 1;
                     if (!String.IsNullOrEmpty(lines[k]))
                     {
-                        ((List<Token?>)tokens).AddRange(Tokenize(lines[k], i));
+                        if (lines[k].Contains("/*") && !lines[k].Contains("*/"))
+                        {
+                            blockcmtMult = true;
+                        }
+                        ((List<Token?>)tokens).AddRange(Tokenize(lines[k], ref blockcmtContent, i, blockcmtMult));
+                        if (lines[k].Contains("*/"))
+                        {
+                            blockcmtMult = false;
+                        }
+                        if (blockcmtMult)
+                        {
+                            blockcmtContent += "\\n";
+                        }
                     }
                 }
             }
             return tokens;
         }
-        public static IList<Token?> Tokenize(string line, int i = 1)
+        public static IList<Token?> Tokenize(string line, int i = 1, bool blockcmtMult = false)
+        {
+            string blockcmtContent = "";
+            
+            return Tokenize(line, ref blockcmtContent, i, blockcmtMult);
+        }
+        public static IList<Token?> Tokenize(string line, ref string blockcmtContent, int i = 1, bool blockcmtMult = false)
         {
             IList<Token?> tokens = new List<Token?>();
             if (!String.IsNullOrEmpty(line))
@@ -32,50 +52,89 @@ namespace TruCompiler.Lexical_Analyzer
                         
                 string[] values = line.Split();
                 bool inlinecmt = false;
+                bool blockcmt = false;
                 string commentContent = "";
                 int charCount = 0;
-                string value;
+                
+                string value = null;
                 for (int j = 0; j < values.Length; j++)
                 {
                     if (!String.IsNullOrEmpty(values[j]))
                     {
-                        value = CheckInlineComments(values[j], ref tokens, i, ref inlinecmt);
+                        if (!blockcmtMult && !blockcmt)
+                        {
+                            value = CheckInlineComments(values[j], ref tokens, i, ref inlinecmt);
+                            value = CheckBlockComments(value, ref tokens, i, ref blockcmt);
+                        }
+                        else
+                        {
+                            value = CheckBlockComments(values[j], ref tokens, i, ref blockcmt);
+                        }
 
-                        if (!inlinecmt)
+
+                        if (!inlinecmt && !blockcmtMult && !blockcmt)
                         {
                             //regular tokens
                             tokens.Add(CreateToken(value, i));
                             charCount += value.Length;
                             charCount++;
-                        } else
+                        } else if (inlinecmt || blockcmtMult || blockcmt)
                         {
                             //This is to add back any \t or whitespaces between the words in the comments from the original line
                             if (line[charCount] == ' ' || line[charCount] == '\t')
                             {
-                                commentContent += line[charCount].ToString() + value;
+                                if (blockcmtMult)
+                                {
+                                    blockcmtContent += line[charCount].ToString() + value;
+                                } else
+                                {
+                                    commentContent += line[charCount].ToString() + value;
+                                }
                                 charCount++;
                             }
                             else
                             {
-                                commentContent += value;
+                                if (blockcmtMult)
+                                {
+                                    blockcmtContent += value;
+                                }
+                                else
+                                {
+                                    commentContent += value;
+                                }
                             }
                             charCount += values[j].Length;
-                        }
+                        } 
                     }
                     else
                     {
-                        if (inlinecmt)
+                        if (inlinecmt || blockcmt)
                         {
                             commentContent += line[charCount].ToString();
                             charCount++;
+                        } else if (blockcmtMult)
+                        {
+                            blockcmtContent += line[charCount].ToString();
+                            charCount++;
                         }
                     }
+                    CheckBlockCommentsEnd(values[j], ref tokens, i, ref blockcmt, blockcmtMult);
                 }
+                
 
                 if (inlinecmt && commentContent.Length > 2)
                 {
                     AddInlineCommentContent(commentContent, ref tokens, i);
+                } else if (!blockcmt && commentContent.Length > 4 && !blockcmtMult)
+                {
+                    AddBlockCommentContent(commentContent, ref tokens, i);
                 }
+                else if (blockcmtMult && !blockcmt && line.Contains("*/"))
+                {
+                    int count = i - blockcmtContent.Split("\\n").Length + 1;
+                    AddBlockCommentContent(blockcmtContent, ref tokens, count);
+                }
+
             }
             return tokens;
         }
