855c8859f2965dd2aa77fb9772d12775621e35d4
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2020-02-13 01:30:02 -0500

    [#1] Added one lasst edge case test

--- TruCompilerTests/Lexical Analyzer/ProgramCodeTests.cs
+++ TruCompilerTests/Lexical Analyzer/ProgramCodeTests.cs
@@ -94,6 +94,43 @@ namespace TruCompilerTests.Lexical_Analyzer
             Assert.AreEqual(">", tokens[5].GetValueOrDefault().Value);
             Assert.AreEqual("x", tokens[6].GetValueOrDefault().Value);
         }
+
+        // Test data: <>::<if==>_x
+        // Regular line of code without spaces with illegal id that should be rejected
+        [TestMethod]
+        public void TestNoSpacesIllegalId()
+        {
+            tokens = LexicalAnalyzer.Tokenize("<>::<if==>_x");
+            Assert.AreEqual(7, tokens.Count);
+            Assert.AreEqual(Lexeme.noteq, tokens[0].GetValueOrDefault().Lexeme);
+            Assert.AreEqual(Lexeme.coloncolon, tokens[1].GetValueOrDefault().Lexeme);
+            Assert.AreEqual(Lexeme.lt, tokens[2].GetValueOrDefault().Lexeme);
+            Assert.AreEqual(Lexeme.keyword, tokens[3].GetValueOrDefault().Lexeme);
+            Assert.AreEqual(Lexeme.eqeq, tokens[4].GetValueOrDefault().Lexeme);
+            Assert.AreEqual(Lexeme.gt, tokens[5].GetValueOrDefault().Lexeme);
+            Assert.AreEqual(Lexeme.id, tokens[6].GetValueOrDefault().Lexeme);
+            Assert.AreEqual(1, tokens[0].GetValueOrDefault().Line);
+            Assert.AreEqual(1, tokens[1].GetValueOrDefault().Line);
+            Assert.AreEqual(1, tokens[2].GetValueOrDefault().Line);
+            Assert.AreEqual(1, tokens[3].GetValueOrDefault().Line);
+            Assert.AreEqual(1, tokens[4].GetValueOrDefault().Line);
+            Assert.AreEqual(1, tokens[5].GetValueOrDefault().Line);
+            Assert.AreEqual(1, tokens[6].GetValueOrDefault().Line);
+            Assert.IsTrue(tokens[0].GetValueOrDefault().IsValid);
+            Assert.IsTrue(tokens[1].GetValueOrDefault().IsValid);
+            Assert.IsTrue(tokens[2].GetValueOrDefault().IsValid);
+            Assert.IsTrue(tokens[3].GetValueOrDefault().IsValid);
+            Assert.IsTrue(tokens[4].GetValueOrDefault().IsValid);
+            Assert.IsTrue(tokens[5].GetValueOrDefault().IsValid);
+            Assert.IsFalse(tokens[6].GetValueOrDefault().IsValid);
+            Assert.AreEqual("<>", tokens[0].GetValueOrDefault().Value);
+            Assert.AreEqual("::", tokens[1].GetValueOrDefault().Value);
+            Assert.AreEqual("<", tokens[2].GetValueOrDefault().Value);
+            Assert.AreEqual("if", tokens[3].GetValueOrDefault().Value);
+            Assert.AreEqual("==", tokens[4].GetValueOrDefault().Value);
+            Assert.AreEqual(">", tokens[5].GetValueOrDefault().Value);
+            Assert.AreEqual("_x", tokens[6].GetValueOrDefault().Value);
+        }
     }
 
 }
